81f138c32b0eb7528af742ea35969516dc57efda
Wendel Damasceno <2022289060015@ifba.edu.br>
2025-04-02 23:52:24 -0300

    ul

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,18 +4,6 @@ import './styles/global.css';
 import './styles/leaflet-custom.css';
 import App from './App.jsx';
 import reportWebVitals from './reportWebVitals';
-import 'framer-motion';
-
-// Adicione as fontes diretamente no <head> do documento
-const addGoogleFonts = () => {
-  const link = document.createElement('link');
-  link.rel = 'stylesheet';
-  link.href = 'https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@400;500;600;700&family=Noto+Sans:wght@400;500;600;700&display=swap';
-  document.head.appendChild(link);
-};
-
-// Chamar a função para adicionar as fontes
-addGoogleFonts();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
